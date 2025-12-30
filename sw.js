288f14b0641e59e565dc91a88d9218cfaff0d6fa
cristiP2 <pomanacristi@yahoo.com>
2025-12-30 15:41:53 +0200

    Prima versiune completa

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'budget-app-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.29/jspdf.plugin.autotable.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+    'https://cdn-icons-png.flaticon.com/512/1077/1077874.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
